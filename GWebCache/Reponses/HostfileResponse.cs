15f063c1925932507887512d260ec4d9f7182ca7
michiel-schoofs <36503058+michiel-schoofs@users.noreply.github.com>
2024-04-13 14:30:18 +0200

    [feat] hostfile
    Finished hostfile response

--- GWebCache/Reponses/HostfileResponse.cs
+++ GWebCache/Reponses/HostfileResponse.cs
@@ -8,17 +8,36 @@ using GWebCache.ReponseProcessing;
 
 namespace GWebCache.Reponses
 {
-    public class HostfileLine
+    public class GnutellaNode
     {
         public IPAddress IPAddress { get; set; }
+        public int port { get; set; }
     }
 
     public class HostfileResponse: GWebCacheResponse { 
-        public List<HostfileLine> HostfileLines { get; set; } = new List<HostfileLine>();
+        public List<GnutellaNode> HostfileLines { get; set; } = new List<GnutellaNode>();
 
+        public override bool IsValidResponse(HttpResponseMessage? responseMessage)
+        {
+            var response = base.IsValidResponse(responseMessage);
+            var content = responseMessage?.Content?.ReadAsStringAsync().Result;
+            return response && !string.IsNullOrWhiteSpace(content) && !content.Contains("error", StringComparison.InvariantCultureIgnoreCase);
+        }
         public override void Parse(HttpResponseMessage? response)
         {
-            throw new NotImplementedException();
+            var content = response?.Content?.ReadAsStringAsync().Result;
+            var lines = content?.Split("\n").Select(l => l.Trim()).Where(l => !string.IsNullOrEmpty(l)).ToArray();
+            foreach(string line in lines) {
+                string[] parts = line.Split(":");
+                if (parts.Length == 2)
+                {
+                    HostfileLines.Add(new GnutellaNode
+                    {
+                        IPAddress = IPAddress.Parse(parts[0]),
+                        port = int.Parse(parts[1])
+                    });
+                }
+            }
         }
     }
 }
