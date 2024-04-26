76ef8bfc3e2b9d11e41340af6807146054d5cb9f
michiel-schoofs <michiel-schoofs@users.noreply.github.com>
2024-04-26 09:42:02 +0000

    deploy: 239322ae9dba8df9116967ac92db815915487c09

--- search/all_3.js
+++ search/all_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['error_20handling_0',['Error handling',['../d3/dcb/md_Examples.html#autotoc_md2',1,'']]],
-  ['examples_1',['Examples',['../d3/dcb/md_Examples.html',1,'']]]
+  ['examples_1',['Examples',['../d3/dcb/md_Examples.html',1,'Examples'],['../index.html#autotoc_md8',1,'Examples']]]
 ];
