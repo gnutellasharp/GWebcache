26a67fbcac96c1be9c197ad94d30a1f187996de2
michiel-schoofs <michiel-schoofs@users.noreply.github.com>
2024-04-29 07:02:04 +0000

    deploy: 3ed61a965b4315ff87552f1c6297ad143c1644ae

--- menudata.js
+++ menudata.js
@@ -24,7 +24,6 @@
 */
 var menudata={children:[
 {text:"Main Page",url:"index.html"},
-{text:"Related Pages",url:"pages.html"},
 {text:"Classes",url:"annotated.html",children:[
 {text:"Class List",url:"annotated.html"},
 {text:"Class Index",url:"classes.html"},
@@ -32,4 +31,5 @@ var menudata={children:[
 {text:"Class Members",url:"functions.html",children:[
 {text:"All",url:"functions.html"},
 {text:"Functions",url:"functions_func.html"},
-{text:"Properties",url:"functions_prop.html"}]}]}]}
+{text:"Properties",url:"functions_prop.html"}]}]},
+{text:"Examples",url:"@ref md__examples"}]}
