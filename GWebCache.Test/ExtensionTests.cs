8a0541a80b834a3248ca93efad348f7d18b10357
michiel-sj <iamjava@hotmail.be>
2024-05-10 11:01:11 +0200

    Feat finished Extension method tests

--- GWebCache.Test/ExtensionTests.cs
+++ GWebCache.Test/ExtensionTests.cs
@@ -8,55 +8,31 @@ public class ExtensionsTest {
 		get {
 			return new[]
 			{
-			[ 
-				new Uri("www.test.com"), 
+			[
+				new Uri("https://www.test.com"),
 				new Dictionary<string, object> { { "test", "yeah" } },
-				"www.test.com?test=yeah"
+				"https://www.test.com/?test=yeah"
 			],
 			[
-				new Uri("www.test.com?test=yeah"),
+				new Uri("https://www.test.com?test=yeah"),
 				new Dictionary<string, object> { { "test", "yeah" } },
-				"www.test.com?test=yeah"
+				"https://www.test.com/?test=yeah"
 			],
 			[
-				new Uri("www.test.com?test=yeah&test=yeah"),
+				new Uri("https://www.test.com?test=yeah&tester=yeah"),
 				new Dictionary<string, object> { { "test", "yeah" } },
-				"www.test.com?test=yeah&test=yeah"
+				"https://www.test.com/?test=yeah&tester=yeah"
 			],
 			[
-				new Uri("www.test.com"),
+				new Uri("https://www.test.com"),
 				new Dictionary<string, object> { { "page", 1}, { "filter", true} },
-				"www.test.com?page=1&filter=true"
+				"https://www.test.com/?page=1&filter=True"
 			],
 			new object[] {
-				new Uri("www.test.com"),
+				new Uri("https://www.test.com"),
 				new Dictionary<string, object>(),
-				"www.test.com"
+				"https://www.test.com/"
 			},
-		  };
-		}
-	}
-
-	public static IEnumerable<object[]> InvalidUrlData {
-		get {
-			return new[]
-			{
-			new object[] {
-				new Uri("www.test.com"),
-				new Dictionary<string, object> { { "test", null } }
-			},
-			[
-				new Uri("www.test.com"),
-				new Dictionary<string, object> { { null, "test"} }
-			],
-			[
-				null,
-				new Dictionary<string, object> { { "ttest", "yeah"} }
-			],
-			[
-				new Uri("www.test.com"),
-				null
-			],
 		  };
 		}
 	}
@@ -74,6 +50,7 @@ public class ExtensionsTest {
 		
 		Assert.AreEqual(response.SplitContentInFields().Length, expectedOutputAmount);
 	}
+
 
 	[TestMethod]
 	[DynamicData(nameof(ValidUrlData))]
