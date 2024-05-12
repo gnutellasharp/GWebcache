3ed6ee5c814f9f9af639dfd075771c6b2077bbef
michiel-schoofs <michiel-schoofs@users.noreply.github.com>
2024-05-12 18:37:25 +0000

    deploy: 5913ceffebf865c869d77e2808dcee4d0dcd075b

--- search/all_b.js
+++ search/all_b.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['request_0',['Preforming an update request',['../d3/dcb/md_Examples.html#autotoc_md5',1,'']]],
   ['result_1',['Result',['../d0/d9d/classGWebCache_1_1ReponseProcessing_1_1Result.html',1,'GWebCache::ReponseProcessing']]],
-  ['resultobject_2',['ResultObject',['../d0/d9d/classGWebCache_1_1ReponseProcessing_1_1Result.html#a94f744940adb29c5a4049c1733f98860',1,'GWebCache::ReponseProcessing::Result']]]
+  ['resultobject_2',['ResultObject',['../d0/d9d/classGWebCache_1_1ReponseProcessing_1_1Result.html#a94f744940adb29c5a4049c1733f98860',1,'GWebCache::ReponseProcessing::Result']]],
+  ['resulttest_3',['ResultTest',['../d5/d2b/classGWebCache_1_1Test_1_1Models_1_1ResultTest.html',1,'GWebCache::Test::Models']]]
 ];
--- search/classes_5.js
+++ search/classes_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['result_0',['Result',['../d0/d9d/classGWebCache_1_1ReponseProcessing_1_1Result.html',1,'GWebCache::ReponseProcessing']]]
+  ['result_0',['Result',['../d0/d9d/classGWebCache_1_1ReponseProcessing_1_1Result.html',1,'GWebCache::ReponseProcessing']]],
+  ['resulttest_1',['ResultTest',['../d5/d2b/classGWebCache_1_1Test_1_1Models_1_1ResultTest.html',1,'GWebCache::Test::Models']]]
 ];
