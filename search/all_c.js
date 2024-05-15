32f92c87b57f326c1e96f78bf69a8b68ca081f82
michiel-schoofs <michiel-schoofs@users.noreply.github.com>
2024-05-15 21:22:29 +0000

    deploy: 1f73fd32c46289f6f0cc5012ac1e2211dd01f4d6

--- search/all_c.js
+++ search/all_c.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['statfileresponse_0',['StatFileResponse',['../de/dc1/classGWebCache_1_1Reponses_1_1StatFileResponse.html',1,'GWebCache::Reponses']]],
-  ['supportednetworks_1',['SupportedNetworks',['../d0/d39/classGWebCache_1_1Reponses_1_1PongResponse.html#a33a18e56b4aaef5979e2ae0328ba03ea',1,'GWebCache::Reponses::PongResponse']]]
+  ['supported_20caches_1',['Supported Caches',['../index.html#autotoc_md9',1,'']]],
+  ['supportednetworks_2',['SupportedNetworks',['../d0/d39/classGWebCache_1_1Reponses_1_1PongResponse.html#a33a18e56b4aaef5979e2ae0328ba03ea',1,'GWebCache::Reponses::PongResponse']]]
 ];
